70dc61393ea8793d6bae5f31f9ee1399380c597b
Dmitrij <dmitrij.korotov@bk.ru>
2024-01-08 16:58:14 +0300

    Task number 2

--- Home_Work.py
+++ Home_Work.py
@@ -11,46 +11,53 @@ class Student:
     def add_courses(self, course_name):
         self.finished_courses.append(course_name)
 
+    def rate_hw(self, lectuer, course, grade):
+        if isinstance(lectuer, Lecturer) and course in lectuer.courses_attached and course in self.courses_in_progress and 1 <= grade <= 10:
+            if  course in lectuer.grades:
+                lectuer.grades[course] += [grade]
+            else:
+                lectuer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 class Mentor:
     
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached =[]
+        
+class Lecturer(Mentor):
+    
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
 
-
-    def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+class Reviewer(Mentor):
+    
+    def rate_hw(self, lectuer, student, course, grade):
+        if isinstance(student, Student) and course in lectuer.courses_attached and course in student.courses_in_progress and 1 <= grade <= 10:
             if  course in student.grades:
                 student.grades[course] += [grade]
             else:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
-        
-class Lecturer(Mentor):
-    pass
-
-class Reviewer(Mentor):
-    pass
 
 best_student = Student('Ruoy', 'Eman', 'Male')
-best_student.finished_courses += ['Git']
+best_student.add_courses('Git')
 best_student.courses_in_progress += ['Python']
 best_student.grades['Git'] = [10, 10, 10, 10, 10]
-best_student.grades['Python'] = [10, 10]
 
 
-print(best_student.finished_courses)
-print(best_student.courses_in_progress)
-print(best_student.grades)
+cool_lectuer = Lecturer('Some', 'Buddy')
+cool_lectuer.courses_attached += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
-print(cool_mentor.courses_attached)
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_reviewer = Reviewer('Ivan', 'Ivanov')
+cool_reviewer.rate_hw(cool_lectuer, best_student, 'Python', 10)
+cool_reviewer.rate_hw(cool_lectuer, best_student, 'Python', 10)
+cool_reviewer.rate_hw(cool_lectuer, best_student, 'Python', 10)
 
 print(best_student.grades)
+print(cool_lectuer.grades)
